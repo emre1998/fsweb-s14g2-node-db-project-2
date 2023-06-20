dcfec589dd120d79ad499fa1266372deac8476cf
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-06-20 19:27:37 +0300

    Cars db-api project is all done

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,26 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // HOKUS POKUS
+  return db("cars");
+};
+
+const getById = (id) => {
+  // HOKUS POKUS
+  return db("cars").where("id",id).first();
+};
+
+const getByVin = (vin)=> {
+  return db("cars").where("vin",vin).first();
 }
 
-const getById = () => {
-  // HOKUS POKUS
-}
 
-const create = () => {
+const create = async (cars) => {
   // HOKUS POKUS
-}
+  let [id]= await db("cars").insert(cars);
+  return getById(id);
+};
+
+module.exports = {
+  getAll,getById,getByVin,create
+};
